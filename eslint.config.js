50f1fa9b72917ab3a7699f10b046662bc9a45b46
Rodrigo Barraza <hello@rod.dev>
2026-03-27 21:23:07 -0700

    refactor: migrate eslint configuration to .js, update linting scripts, and refine editor settings

--- eslint.config.js
+++ eslint.config.js
@@ -20,14 +20,7 @@ const eslintConfig = defineConfig([
       "no-var": "error",
     },
   },
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
 ]);
 
 export default eslintConfig;
