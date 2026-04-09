efb765d2f908965ea9c9906da2d2443f566514e8
Rodrigo Barraza <hello@rod.dev>
2026-04-08 18:21:32 -0700

    refactor: move agent identifier from request headers to payload body in PrismService

--- src/services/PrismService.js
+++ src/services/PrismService.js
@@ -9,7 +9,6 @@ function getHeaders() {
   return {
     "Content-Type": "application/json",
     "x-project": PROJECT_NAME,
-    "x-agent": "CODING",
   };
 }
 
@@ -551,7 +550,7 @@ export default class PrismService {
    * @returns {Promise<object>}
    */
   static async generateAgentText(payload) {
-    return PrismService._request("/agent?stream=false", { body: payload });
+    return PrismService._request("/agent?stream=false", { body: { ...payload, agent: "CODING" } });
   }
 
   /**
@@ -754,7 +753,7 @@ export default class PrismService {
    * @returns {Function} abort - Call to cancel the stream early
    */
   static streamAgentText(payload, callbacks) {
-    return PrismService._streamSSE("/agent", { body: payload }, callbacks);
+    return PrismService._streamSSE("/agent", { body: { ...payload, agent: "CODING" } }, callbacks);
   }
 
   /**
